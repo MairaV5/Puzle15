55a033d20cdcf87f1f0a6a7031f141b496341009
Maira Vecele <mairavecele6@gmail.com>
2020-07-01 17:15:52 +0300

    version 17:15

--- PuzzleArea.cs
+++ PuzzleArea.cs
@@ -39,8 +39,14 @@ namespace Puzle15
                     block.Top = row * 87;
                     block.Left = col * 87;
                     block.Text = blockCount.ToString();
+                    Name = "Block" + blockCount.ToString();
+
+                    //block.Click += new EventHandler(Block_Click);
+                    block.Click += Block_Click;
+
                     if(blockCount == 16)
                     {
+                        block.Name = "EmptyBlock";
                         block.Text = string.Empty;
                         block.BackColor = Color.Gray;
                         block.FlatStyle = FlatStyle.Flat;
@@ -56,5 +62,51 @@ namespace Puzle15
             }
         }
 
+        private void Block_Click(object sender, EventArgs e)
+        {
+            Button block = (Button)sender;
+            //MessageBox.Show(block.Name);
+            if (IsAdjacent(block))
+            {
+                SwapBlocks(block);
+            }
+        }
+
+        private void SwapBlocks(Button block)
+        {
+            Button emptyBlock = (Button)this.Controls["EmptyBlock"];
+            Point oldLocation = block.Location;
+            block.Location = emptyBlock.Location;
+            emptyBlock.Location = oldLocation;
+            // these both are the same solutions but different lenght
+            //int oldLeft = block.Left;
+            //int oldTop = block.Top;
+            //block.Left = emptyBlock.Left;
+            //block.Top = emptyBlock.Top;
+            //emptyBlock.Left = oldLeft;
+            //emptyBlock.Top = oldTop;
+
+        }
+
+        private bool IsAdjacent(Button block)
+        {
+            double a;
+            double b;
+            double c;
+            Button emptyBlock = (Button)this.Controls["EmptyBlock"];
+
+            a = Math.Abs(emptyBlock.Top - block.Top);
+            b = Math.Abs(emptyBlock.Left - block.Left);
+            c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            if(c < 88)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //Math.Abs- modulis
+        }
     }
 }
